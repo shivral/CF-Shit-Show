8116f66b319d23e46c795526cceeabfc33a2acc4
shivral <79295827+shivral@users.noreply.github.com>
2021-05-27 11:09:43 +0530

    ooh boi

---
+++ temp1.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+int32_t mod = 1e9 + 7;
+#define MAX_N 100001
+auto cmp = [](int left, int right) { return (right ) < (left ); }; //descending
+auto cmp2 = [](int left, int right) { return (left ) < (right); }; //ascending
+vector<vector<int>> tree;
+vector<bool> vis;
+
+
+void solve(){
+//     int n,m;
+    
+//     // cin>>n>>m;
+//     tree=vector<vector<int>>(n+1);
+//     vector<int> ans{5,3,1,2};
+//     vis=vector<bool>(n+1,false);
+//     priority_queue<int, vector<int>, decltype(cmp)> q(cmp);
+   
+//     sort(ans.begin(),ans.end(),cmp2);
+//    for(auto e: ans){
+//        cout<<e<<" ";
+//    }
+
+
+}
+
+
+signed main(){
+    
+    fastIO;
+   
+    solve();
+    return 0;
+}
