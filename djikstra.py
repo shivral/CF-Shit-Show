3fc7c429945d4d1cdc66b954135def85d19f8374
shivral <79295827+shivral@users.noreply.github.com>
2021-06-14 20:48:27 +0530

    Update djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -1,30 +1,29 @@
 from collections import defaultdict
-import  heapq
+import heapq
 from sys import stdin
-input=stdin.readline
+
+input = stdin.readline
 
 g = defaultdict(list)
-n, m, k = map(int, input().strip().split())
+n, m = map(int, input().strip().split())
 for i in range(m):
 	x, y, w = map(int, input().strip().split())
-	g[x].append((w,y)) #weight : dest for heapq shit
-d=defaultdict(list)
-q=[(0,1)]
-p=[0]*(n+1)
+	g[x].append((w, y))  # weight : dest for heapq shit
+d = [float("inf")]*(n+1)
+d[1]=0
+q = [(0, 1)]
+p = [0] * (n + 1)
 heapq.heapify(q)
 while q:
-	u=heapq.heappop(q)
-	if len(d[u[1]])>=k:
-		continue
-		
+	u = heapq.heappop(q)
+
+
 	for child in g[u[1]]:
-		to=child[1]
-		wt=child[0]
-		if d[u[1]]+wt<d[to]:
-			d[to]=d[u[1]]+wt
-			p[to]=u[1]
-			heapq.heappush(q,(d[to],to))
-			
+		to = child[1]
+		wt = child[0]
+		if d[u[1]] + wt < d[to]:
+			d[to] = d[u[1]] + wt
+			p[to] = u[1]
+			heapq.heappush(q, (d[to], to))
 
-
-print(*d[n])
+print(d)
