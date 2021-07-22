6ef8237d0f2662bf6f499ff2b1c26787074aa233
shivral <79295827+shivral@users.noreply.github.com>
2021-07-22 16:32:17 +0530

    Update lis_BIT.py

--- lis_BIT.py
+++ lis_BIT.py
@@ -4,37 +4,45 @@ input = stdin.readline
 
 
 class BIT():
-    def __init__(self, n):
-        self.n = n
-        self.tree = [0] * (n + 1)
+	def __init__(self, n):
+		self.n = n
+		self.tree = [0] * (n + 1)
 
-    def sum(self, i):
-        ans = 0
-        i += 1
-        while i > 0:
-            ans =max(ans, self.tree[i])
-            i -= (i & (-i))
-        return ans
+	def sum(self, i):
+		ans = 0
+		i += 1
+		while i > 0:
+			ans = max(ans, self.tree[i])
+			i -= (i & (-i))
+		return ans
 
-    def update(self, i, value):
-        i += 1
-        while i <= self.n:
-            self.tree[i] = max(value,self.tree[i])
-            i += (i & (-i))
+	def update(self, i, value):
+		i += 1
+		while i <= self.n:
+			self.tree[i] = max(value, self.tree[i])
+			i += (i & (-i))
+
+
 def f(a):
-    newind=0
-    maxs=0
-    ans=0
-    ft=BIT(2*(10**5)+5)
-    for i in a:
-        maxs=ft.sum(i-1)
-        # print(maxs,i)
-        ft.update(i,maxs+1)
-        ans=max(ans,maxs+1)
-        # print(ft.tree)
+	newind = 0
+	maxs = 0
+	ans = 0
+	ft = BIT(2 * (10 ** 5) + 5)
+	ind = {}
+	maxind=0
+	for i in sorted(list(set(a))):
+		ind[i] = maxind
+		maxind+=1  #index compression
+	for i in a:
+		maxs = ft.sum(ind[i] - 1)
+		# print(maxs,i)
+		ft.update(ind[i], maxs + 1)
+		ans = max(ans, maxs + 1)
+		# print(ft.tree)
 
-    return ans
+	return ans
 
-a=input()
-l=list(map(int,input().strip().split()))
+
+a = input()
+l = list(map(int, input().strip().split()))
 print(f(l))
