fe776982c5d32da351cbe28b4722858c3faeca51
shivral <79295827+shivral@users.noreply.github.com>
2022-06-25 23:55:33 +0530

    Update rmq.cpp

--- rmq.cpp
+++ rmq.cpp
@@ -1,32 +1,32 @@
 class SparseTable{
     private:
-        vector<vector<ll>> Table,Table1;
-        vector<ll> log;
+        vector<vector<int>> Table,Table1;
+        vector<int> log;
     public:
-        SparseTable(vector<ll> arr){
-            ll n = arr.size();
-            ll k = log2(n);
-            Table.resize(k+1,vector<ll>(n,0));
-            Table1.resize(k+1,vector<ll>(n,0));
+        SparseTable(vector<int> arr){
+            int n = arr.size();
+            int k = log2(n);
+            Table.resize(k+1,vector<int>(n,0));
+            Table1.resize(k+1,vector<int>(n,0));
             log.resize(n+1);
             Table[0] = arr;
             Table1[0] = arr;
-            for(ll i=1;i<=k;i++){
-                for(ll j=0;j<=n-(1LL<<i);j++){
+            for(int i=1;i<=k;i++){
+                for(int j=0;j<=n-(1LL<<i);j++){
                     Table[i][j] = min(Table[i-1][j],Table[i-1][j+(1LL<<(i-1LL))]);
                     Table1[i][j] = max(Table1[i-1][j],Table1[i-1][j+(1LL<<(i-1LL))]);
                 }
             }
-            for(ll i=1;i<=n;i++){
+            for(int i=1;i<=n;i++){
                 log[i] = log2(i);
             }
         }
-        long long int queryMIN(ll l,ll r){
-            ll k = log[r-l+1];
+        int queryMIN(int l,int r){
+            int k = log[r-l+1];
             return min(Table[k][l-1],Table[k][r-(1LL<<k)]);
         }
-        long long int queryMAX(ll l,ll r){
-            ll k = log[r-l+1];
+        int queryMAX(int l,int r){
+            int k = log[r-l+1];
             return max(Table1[k][l-1],Table1[k][r-(1LL<<k)]);
         }
 };
