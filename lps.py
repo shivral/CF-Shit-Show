3a03a201d453b0e69d6201704f727041cc031477
shivral <79295827+shivral@users.noreply.github.com>
2023-03-28 11:12:34 +0530

    Update lps.py

--- lps.py
+++ lps.py
@@ -12,3 +12,17 @@ def LPS(S):
                 
             
         return dp[0][S.length()-1];
+
+
+class Solution:
+    def countPalindromicSubsequences(self, s: str) -> int:
+    dp=[[0]*len(s) for i in range(len(s))]
+    for l in range(len(s)-1,-1,-1):
+        dp[l][l]=1
+        for r in range(l+1,len(s)):
+            if s[l]==s[r]:
+                dp[l][r]+=dp[l][r-1]+1+dp[l+1][r]
+            else:
+                dp[l][r]+=dp[l][r-1]+dp[l+1][r]-dp[l+1][r-1]
+        print(dp)
+        return 1
